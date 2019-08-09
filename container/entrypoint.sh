e75b882ee770cdbc002bdee03c7c68c70f3b7ee8
cryi <cryi@tutanota.com>
2019-08-10 00:54:26 +0200

    adjusted launch args

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/sbin/geth-xero --syncmode=fast --cache=512 --rpc --rpcaddr "0.0.0.0" --rpcvhosts="mn,localhost"
+/usr/sbin/geth-xero --syncmode fast -node --rpc --rpcaddr "0.0.0.0" --rpcvhosts="mn,localhost"
